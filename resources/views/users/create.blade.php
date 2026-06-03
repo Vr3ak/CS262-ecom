d854b13ba6d0e3cf63697113f712bc788b62ed05
Vr3ak <vireakheng0@gmail.com>
2026-06-03 20:30:12 +0700

    Finish User Crud

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,40 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Create User</h1>
+
+    <div>
+        @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $errors)
+                <li>{{$errors}}</li>
+            @endforeach
+        </ul>
+        @endif
+    </div>
+
+    <form method="post" action="{{route('users.store')}}">
+        @csrf
+        <div>
+            <label>Full Name</label>
+            <input type="text" name="name"/>
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="text" name="email"/>
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password" name="password"/>
+        </div>
+        <div>
+            <input type="submit" value="Save User">
+        </div>
+    </form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', [UserController::class, 'index'])->name('users.index');
+Route::get('/user/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/user', [UserController::class, 'store'])->name('users.store');
+Route::get('/user/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::put('/user/{user}/update', [UserController::class, 'update'])->name('users.update');
+Route::delete('/user/{user}/destroy', [UserController::class, 'destroy'])->name('users.destroy');
